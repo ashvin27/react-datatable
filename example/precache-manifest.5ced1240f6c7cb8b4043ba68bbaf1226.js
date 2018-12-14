8de52331e5b0654e11aa64f0c038cfec40ac6215
ashvin.patel <ashvin.patel@digivalet.com>
2018-12-14 16:34:58 +0530

    Add example

---
+++ example/precache-manifest.5ced1240f6c7cb8b4043ba68bbaf1226.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d9ed16c0447a8163f0ab",
+    "url": "/static/js/main.d9ed16c0.chunk.js"
+  },
+  {
+    "revision": "1aa0b7cc411921afdd47",
+    "url": "/static/js/1.1aa0b7cc.chunk.js"
+  },
+  {
+    "revision": "1b4f1a045e0c055608297ce7f6023514",
+    "url": "/index.html"
+  }
+];
