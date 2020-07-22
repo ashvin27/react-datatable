eae7898551928a3d2fc54391d781b62111d0b497
Ashvin Patel <patelash212@gmail.com>
2020-07-22 19:26:48 +0530

    Bug fixes: print and export issue while searching #42

--- example/src/UserList.js
+++ example/src/UserList.js
@@ -143,7 +143,7 @@ class UserList extends Component {
                 loading: false,
                 users: users
             })
-        }, 5000);
+        }, 3000);
     }
 
     editUser(user) {
@@ -162,6 +162,7 @@ class UserList extends Component {
         return (
             <div>
                 <ReactDatatable
+                    className="table table-bordered table-striped custom-class"
                     config={this.config}
                     records={this.state.users}
                     columns={this.columns}
